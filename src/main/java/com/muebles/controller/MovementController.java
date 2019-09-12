971e1dc9534696a70022f4f755df73fe46a1e8f1
eveloso-src <elemudela@gmail.com>
2019-09-12 00:52:22 -0300

    Date fix

--- src/main/java/com/muebles/controller/MovementController.java
+++ src/main/java/com/muebles/controller/MovementController.java
@@ -1,17 +1,12 @@
 package com.muebles.controller;
 
-import java.sql.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import javax.validation.Valid;
+import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,49 +16,26 @@ import com.muebles.service.MovementService;
 @Controller
 public class MovementController {
 
+//	private static final String DATE_PATTERN = "yyyy-MM-dd";
 	@Autowired
 	MovementService movementService;
 
 	@RequestMapping(value = "/movement", method = RequestMethod.POST)
-	@ResponseBody
-	public ModelAndView saveMovement(@RequestParam (value = "date") String dateInput,
-			@RequestParam (value = "code") String code,
-			@RequestParam (value = "color") String color,
-			@RequestParam (value = "amount") int amount,
-			@RequestParam (value = "m2") int m2,
-			@Valid Movement mov, 
-			BindingResult bindingResult
-			) {
+	public @ResponseBody ModelAndView saveMovement(Movement mov, BindingResult bindingResult) {
 		ModelAndView modelAndView = new ModelAndView();
-//		System.out.println("test movement ok " );
-//		System.out.println("code " + code);
-//		System.out.println("color " + color);
-//		System.out.println("amount " + amount);
-//		System.out.println("m2 " + m2);
-//		System.out.println("dateInput " + dateInput);
-//		modelAndView.addObject(getNewMovement());
-		Movement movEnt = new Movement();
-		movEnt.setAmount(amount);
-		movEnt.setCode(code);
-		movEnt.setColor(color);
-		movEnt.setM2(m2);
-
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		try {
-			movEnt.setDate(new Date(sdf.parse(dateInput).getTime()));
-		} catch (ParseException e) {
-			movEnt.setDate(new Date(System.currentTimeMillis()));
+		if (!bindingResult.hasErrors()) {
+			movementService.save(mov);
+		} else {
+			System.out.println("error save movement");
 		}
-		
-		movementService.save(movEnt);
-		
+
 		modelAndView.setViewName("admin/home");
 		return modelAndView;
 	}
 
 	public static Movement getNewMovement() {
 		Movement mov = new Movement();
-		mov.setDate(new Date(System.currentTimeMillis()));
+		mov.setDate(LocalDate.now());
 		return mov;
 	}
 
--- src/main/java/com/muebles/model/Movement.java
+++ src/main/java/com/muebles/model/Movement.java
@@ -1,8 +1,6 @@
 package com.muebles.model;
 
-import java.sql.Date;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -23,13 +22,18 @@ public class Movement {
 	@Column(name = "mov_id")
 	private int id;
 
+//	@Column(name = "date")
+//	@DateTimeFormat(pattern = "dd.MM.yyyy")
+//	@NotEmpty(message = "*Ingrese la fecha")
+//	private java.sql.Date date;
+	@Length(min = 10, message = "*Debe tener 10 caracteres")
 	@Column(name = "date")
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
-//	@NotEmpty(message = "*Ingrese la fecha")
-	private Date date;
+	@NotEmpty(message = "*Ingrese la fecha")
+	private LocalDate date;
 
 	@Column(name = "code")
-//	@Length(min = 5, message = "*Debe tener 5 caracteres")
+	@Length(min = 5, message = "*Debe tener 5 caracteres")
 	@NotEmpty(message = "*Ingrese codigo")
 	private String code;
 	
@@ -55,11 +59,19 @@ public class Movement {
 		this.id = id;
 	}
 
-	public Date getDate() {
+//	public java.sql.Date getDate() {
+//		return date;
+//	}
+//
+//	public void setDate(java.sql.Date date) {
+//		this.date = date;
+//	}
+
+	public LocalDate getDate() {
 		return date;
 	}
 
-	public void setDate(Date date) {
+	public void setDate(LocalDate date) {
 		this.date = date;
 	}
 
