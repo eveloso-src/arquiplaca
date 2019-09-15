4432318facce9f0b2ba70b20c482358bfd0086eb
eveloso-src <elemudela@gmail.com>
2019-09-14 23:42:35 -0300

    id product

--- src/main/java/com/muebles/model/Product.java
+++ src/main/java/com/muebles/model/Product.java
@@ -18,7 +18,7 @@ public class Product {
 	@Id
 //	@GeneratedValue(strategy = GenerationType.AUTO)
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="products_id_seq")
-    @SequenceGenerator(name="products_id_seq", sequenceName="products_id_seq", allocationSize=1)
+    @SequenceGenerator(name="products_id_seq", sequenceName="products_id_seq", allocationSize=1, initialValue=1000)
 	@Column(name = "prod_id")
 	private int id;
 
